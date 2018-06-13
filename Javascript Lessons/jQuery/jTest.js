5b265795944f6348803b441f935f62a0dabe3d57
Your Name <aldimaraya@gmail.com>
2018-06-13 17:40:07 -0400

    JQuery!

---
+++ Javascript Lessons/jQuery/jTest.js
@@ -0,0 +1,11 @@
+//Select all divs and turn purple
+$("div").css("background", "purple");
+
+//Select divs with class "highlight" and make them 200px wide
+$(".highlight").css("width", "200px");
+
+//Select divs with id "third" and give it an orange border
+$("#third").css("border", "4px solid orange");
+
+//Select only the first div and change its font color to pink
+$("div:first-of-type").css("color", "pink");
